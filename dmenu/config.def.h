9d5d443f2efb6e31adba6291233eacf357f9d6dc
Matteo Giorgi <matteo.giorgi@protonmail.com>
2022-05-15 10:09:55 +0200

    new dimmed background

--- dmenu/config.def.h
+++ dmenu/config.def.h
@@ -10,8 +10,8 @@ static const char *prompt      = NULL;      /* -p  option; prompt to the left of
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#FFFFFF", "#000000" },             /* "#bbbbbb", "#222222" */
-	[SchemeSel] = { "#FFFFFF", "#1A1A1A" },              /* "#eeeeee", "#005577" */
-	[SchemeSelHighlight] = { "#FF00FF", "#1A1A1A" },     /* "#ffc978", "#005577" */ 
+	[SchemeSel] = { "#FFFFFF", "#262626" },              /* "#eeeeee", "#005577" */
+	[SchemeSelHighlight] = { "#FF00FF", "#262626" },     /* "#ffc978", "#005577" */ 
 	[SchemeNormHighlight] = { "#FF00FF", "#000000" },    /* "#ffc978", "#222222" */
 	[SchemeOut] = { "#000000", "#00ffff" },
 	[SchemeOutHighlight] = { "#ffc978", "#00ffff" },
