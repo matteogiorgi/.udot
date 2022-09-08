3e8fbac4327739acd8fa50f47a9cc697979fc545
Matteo Giorgi <matteo.giorgi@protonmail.com>
2022-09-09 00:01:22 +0200

    new colors

--- slock/config.def.h
+++ slock/config.def.h
@@ -3,9 +3,9 @@ static const char *user  = "nobody";
 static const char *group = "nogroup";
 
 static const char *colorname[NUMCOLS] = {
-	[INIT] =   "black",     /* after initialization */
-	[INPUT] =  "#005577",   /* during input */
-	[FAILED] = "#CC3333",   /* wrong password */
+	[INIT] =   "#333333",   /* after initialization */
+	[INPUT] =  "#285577",   /* during input */
+	[FAILED] = "#900000",   /* wrong password */
 };
 
 /* treat a cleared input like a wrong password (color) */
