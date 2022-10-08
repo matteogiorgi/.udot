27b7dd55e16b50bfdecefb774c4892deb73e1962
Matteo Giorgi <matteo.giorgi@protonmail.com>
2022-10-08 02:31:25 +0200

    update

--- setup.sh
+++ setup.sh
@@ -158,7 +158,7 @@ install_chrome () {
     [[ ! -d ~/Downloads ]] && mkdir -p ~/Downloads
     cd ~/Downloads
     wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-    sudo dpkg -i google-chrome-stable_current_amd64.deb
+    sudo apt install ./google-chrome-stable_current_amd64.deb
     cd -
 }
 
